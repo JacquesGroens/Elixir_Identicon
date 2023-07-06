0669a8b1eea64aede96d8bba9aaa3252d7a40aac
Jacques <jacquesgroens@gmail.com>
2023-07-06 10:02:44 +0200

    Intial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,50 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(input) do
+
+    input
+    |> hash_input()
+    |> pick_color()
+    |> build_grid()
+
+  end
+
+  def build_grid(image) do
+
+
+
+  end
+
+  def pick_color(image) do
+
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    %Identicon.Image{image | color: {r, g, b}}
+
+  end
+
+  def hash_input(input) do
+
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+
+  end
+
+end
