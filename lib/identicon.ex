235feb9543d0b2f029350873e1a071f6d3135086
Jacques <jacquesgroens@gmail.com>
2023-08-15 10:02:31 +0200

    Build grid and filter odd

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -22,13 +22,43 @@ defmodule Identicon do
     |> hash_input()
     |> pick_color()
     |> build_grid()
+    |> filter_odd_squares
+
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+
+    #Enum.filter(grid, fn(square) -> end)
+    grid = Enum.filter grid, fn({code, _index}) ->
+
+      rem(code, 2) == 0
+
+    end
+
+    %Identicon.Image{image | grid: grid}
 
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
 
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
 
+    % Identicon.Image{image | grid: grid}
 
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+
+    [first, second | _tail] = row
+    row ++ [second, first]
+
+    # [145, 46, 200, 46, 145]
   end
 
   def pick_color(image) do
